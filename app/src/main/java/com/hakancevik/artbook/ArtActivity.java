cf88957248f7e401ea95e6613ac0718c3399f3a0
Hakan Çevik <hakanxcevik7@gmail.com>
2022-06-28 03:26:57 +0300

    Permission Rationale added

--- app/src/main/java/com/hakancevik/artbook/ArtActivity.java
+++ app/src/main/java/com/hakancevik/artbook/ArtActivity.java
@@ -1,16 +1,39 @@
 package com.hakancevik.artbook;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.hakancevik.artbook.databinding.ActivityArtBinding;
+
+import java.io.IOException;
 
 
 public class ArtActivity extends AppCompatActivity {
 
     private ActivityArtBinding binding;
+
+    Bitmap selectedImage;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +41,8 @@ public class ArtActivity extends AppCompatActivity {
         binding = ActivityArtBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        registerLauncher();
 
 
     }
@@ -32,8 +57,84 @@ public class ArtActivity extends AppCompatActivity {
     public void selectImage(View view) {
 
         //photo permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //request permission
+
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                    }
+                }).show();
 
 
+            } else {
+                // request permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            }
+
+        } else {
+            // go to gallery
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
+
+        }
+
+
+    }
+
+
+    public void registerLauncher() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null) {
+                        Uri imageData = intentFromResult.getData();
+                        //binding.imageView.setImageURI(imageData); // we want to data not source for database
+                        try {
+
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                ImageDecoder.Source source = ImageDecoder.createSource(ArtActivity.this.getContentResolver(), imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedImage);
+
+                            } else {
+                                selectedImage = MediaStore.Images.Media.getBitmap(ArtActivity.this.getContentResolver(), imageData);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        });
+
+
+        //permissions process
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result) {
+                    //permission granted
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+
+                } else {
+                    //permission denied
+                    Toast.makeText(ArtActivity.this, "Permission needed!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
     }
 
 
